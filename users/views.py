0d86dde061bd277caf08f8309b69977a5b91c4bc
Lucas Abreu <lucasabreuapi@gmail.com>
2017-07-21 02:02:53 -0300

    cria uma rota na API que devolve todos os usuários do banco

--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from users.models import User
+from users.serializers import UserSerializer
 
-from django.shortcuts import render
+@csrf_exempt
+def user_list(request):
+    """
+    """
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
-# Create your views here.
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResonse(serializer.data, status=201)
+        return JsonReponse(serializer.erros, status=400)
