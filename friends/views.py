e60b6aea4e3fa362172f30d1de063092c29b7b28
Lucas Abreu <lucasabreuapi@gmail.com>
2017-07-25 21:54:26 -0300

    retira ipdb

--- friends/views.py
+++ friends/views.py
@@ -3,6 +3,5 @@ from django.http import JsonResponse
 from repositories import FriendsRepository
 
 def friends(request, **kwargs):
-    import ipdb; ipdb.set_trace()
     friends = FriendsRepository(user_id=kwargs['user_id']).get_all_friends()
     return JsonResponse(friends, status=201 ,safe=False)
