0a23cba32ef6d131d02b6049d03c6603497e3f6e
danitlf <dani@MacBook-Air-de-daniel.local>
2017-08-02 22:54:33 -0300

    admin inclused

--- facecrush/urls.py
+++ facecrush/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^users/$', include('users.urls')),
     url(r'^friends/', include('friends.urls')),
     url(r'^matchs/', include('matchs.urls'))
